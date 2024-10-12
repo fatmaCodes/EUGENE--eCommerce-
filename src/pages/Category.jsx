a9c7ee97bde97dc9eb23fc5e2c4353685cafd2eb
fatmaCodes <fatimakhan39600@gmail.com>
2024-10-12 21:39:49 +0530

    worked on category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,23 +1,59 @@
 import Card from "../component/Card"
 import Blonde from '../assets/blondeHead.jpg'
 import fashion from "../assets/fashion.webp"
-
-
+import browns from "../assets/browns.jpg"
+import mix from "../assets/mix.jpg"
+import brownn from "../assets/brownn.png"
+import reds from "../assets/reds.png"
+import silver from "../assets/silver.png"
+import fantasy from "../assets/fantasy.png"
+import black from "../assets/black.png"
+import haircare from "../assets/haircare.png"
+import all from "../assets/all.png"
+import port from "../assets/port.png"
+import stream from "../assets/stream.png"
+import articles from "../assets/articles.png"
+import help from "../assets/help.png"
+import gift from "../assets/gift.png"
 function CategoryPage() {
   return (
-   <>
-   <div className="h-full w-full flex flex-col items-center gap-5 p-5">
-      <h1 className="text-slate-100 ">
-        COLOR FAMILY
-      </h1>
-      <div className="w-full p-2 h-[25%] flex gap-16 justify-center">
-        <Card img={Blonde} text="BLONDE"/>
-        <Card img={fashion} text="FASHION"/>
-        <Card/>
-        <Card/>
+    <>
+      <div className="h-full w-full flex flex-col items-center gap-5 p-5">
+        <h1 className="text-slate-100 ">
+          <b> COLOR FAMILY </b>
+        </h1>
+        <div className="w-full p-2 h-[25%] flex gap-16 justify-center">
+          <Card img={Blonde} text="BLONDES" />
+          <Card img={fantasy} text="FANTASY SHADES" />
+          <Card img={brownn} text="BROWNS" />
+          <Card img={reds} text="REDS & PLUMS" />
+        </div>
+        <div className="w-full p-2 h-[25%] flex gap-16 justify-center">
+          <Card img={silver} text="GREY & SILVER" />
+          <Card img={black} text="BLACK" />
+          <Card img={haircare} text="MAINTENANCE" />
+          <Card img={all} />
+        </div>
+
+        <div className="border-t-2 border-l-sky-100 p-3">
+            <h1 className="w-full text-center text-slate-100 ">
+              MORE
+            </h1>
+          <div className="w-full p-2 h-[25%] flex gap-16  items-center justify-center">
+            <div className="w-full p-2 h-[25%] flex gap-16 justify-center">
+              <Card img={port} />
+              <Card img={stream} />
+              <Card img={articles} />
+            </div>
+          </div>
+          <div className="w-full p-2 h-[25%] flex gap-16 justify-center">
+            <Card img={help} />
+            <Card img={gift} />
+
+          </div>
+        </div>
       </div>
-   </div>
-   </>
+    </>
   )
 }
 
