733d1f3bc9d7c036f0215094c3ab7f10e09bfc9e
kancherish <cherishkansara4445@gmail.com>
2024-10-12 22:54:09 +0530

    page routing phase 1 started and midway

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import Homepage from './pages/Homepage.jsx'
+import Login from "./pages/Login.jsx"
+import Signup from "./pages/Signup.jsx"
+import CategoryPage from "./pages/Category.jsx"
+import Wishlist from "./pages/WishList.jsx"
 
 const router = createBrowserRouter([
   {
@@ -13,7 +17,23 @@ const router = createBrowserRouter([
       {
         path:"/",
         element:<Homepage/>
-      }
+      },
+      {
+        path:"/login",
+        element:<Login/>
+      },
+      {
+        path:"/signup",
+        element:<Signup/>
+      },
+      {
+        path:"/categories",
+        element:<CategoryPage/>
+      },
+      {
+        path:"/wishlist",
+        element:<Wishlist/>
+      },
     ]
   }
 ])
