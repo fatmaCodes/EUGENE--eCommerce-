f0a430fdfcdad3493f00dbc86316099835e7c86c
kancherish <cherishkansara4445@gmail.com>
2024-10-13 14:09:37 +0530

    added authlayout

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Homepage from './pages/Homepage.jsx'
 import Login from "./pages/Login.jsx"
 import Signup from "./pages/Signup.jsx"
@@ -12,47 +11,60 @@ import Cart from './pages/Cart.jsx'
 import CustomProduct from './pages/CustomProduct.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import AuthLayout from './component/AuthLayout.jsx'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
-        path:"/",
-        element:<Homepage/>
+        path: "/",
+        element: <Homepage />
       },
       {
-        path:"/login",
-        element:<Login/>
+        path: "/login",
+        element: <Login />
       },
       {
-        path:"/signup",
-        element:<Signup/>
+        path: "/signup",
+        element: <Signup />
       },
       {
-        path:"/categories",
-        element:<CategoryPage/>
+        path: "/categories",
+        element: <AuthLayout>
+          <CategoryPage />
+        </AuthLayout>
+
       },
       {
-        path:"/wishlist",
-        element:<Wishlist/>
+        path: "/wishlist",
+        element: <AuthLayout>
+          <Wishlist />
+        </AuthLayout>
+
       },
       {
-        path:"/cart",
-        element:<Cart/>
+        path: "/cart",
+        element: <AuthLayout>
+          <Cart />
+        </AuthLayout>
+
       },
       {
-        path:"/categories/:categ",
-        element:<CustomProduct/>
+        path: "/categories/:categ",
+        element: <AuthLayout>
+          <CustomProduct />
+        </AuthLayout>
+
       }
     ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>  
-     <RouterProvider router={router}/>
+  <Provider store={store}>
+    <RouterProvider router={router} />
   </Provider>
- 
+
 )
